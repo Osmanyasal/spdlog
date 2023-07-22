13458e610804b3502c3cab93c778aa41ff450335
rt7 <oyasal@gmail.com>
2023-07-22 20:53:18 +0300

    install.sh created

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CSPDLOG="Custom SPDLOG"
+
+if [ $# -gt 0 ]; then
+    if [ "$1" = "--help" ]; then
+        echo "$0 [clean]"
+        exit 0
+    fi
+    if [ "$1" = "clean" ]; then
+        echo "${CSPDLOG} executing make clean"
+        rm -rf ./build
+        exit 0
+    else
+        echo "Unknown parameter: $1"
+        exit 1
+    fi
+fi
+
+git checkout v1.x
+mkdir -p build
+echo "${CSPDLOG} build folder created"
+cd build
+cmake .. && make -j
+echo "${CSPDLOG} cmake executed" 
+cd build
+make
